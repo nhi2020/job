a374d8f99e4da26ad220d2e2a319a12b52bcf123
kyul <kyul@DESKTOP-F4U32L8>
2020-10-14 14:49:44 +0900

    00

--- src/main/java/com/job/user/biz/service/BizUserService.java
+++ src/main/java/com/job/user/biz/service/BizUserService.java
@@ -1,5 +1,6 @@
 package com.job.user.biz.service;
 
 public interface BizUserService {
-
+	
+	public BizUserVO b_info(BizUserVO vo)throws Exception;
 }
--- src/main/java/com/job/user/biz/web/BizUserController.java
+++ src/main/java/com/job/user/biz/web/BizUserController.java
@@ -1,5 +1,24 @@
 package com.job.user.biz.web;
 
-public class BizUserController {
+import javax.annotation.Resource;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.job.user.biz.service.BizUserService;
+import com.job.user.biz.service.BizUserVO;
+
+@Controller
+public class BizUserController {
+	
+	@Resource(name="bizUserService")
+	private BizUserService bizUserService;
+	
+	@RequestMapping(value="/user/biz/info/bizInfo.do")
+	public String bizInfo(Model model, BizUserVO vo) throws Exception {
+		BizUserVO bizInfo=bizUserService.b_info(vo);
+		model.addAttribute("bizInfo", bizInfo);
+		return "user/biz/info/bizInfo";
+	}
 }
