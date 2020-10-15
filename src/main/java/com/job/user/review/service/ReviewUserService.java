527c69ddf0365b9424f1004a9e353b4cff09cf25
82103 <82103@DESKTOP-CGC035B>
2020-10-15 10:16:19 +0900

    태형

--- src/main/java/com/job/user/review/service/ReviewUserService.java
+++ src/main/java/com/job/user/review/service/ReviewUserService.java
@@ -4,17 +4,39 @@ import java.util.List;
 
 
 public interface ReviewUserService {
+	/*리뷰리스트*/
+	/*-기업리뷰*/
+	public List<ReviewUserVO> reviewSelectList1(ReviewUserVO reviewUserVO);
+	public int total1();
+	/*-기업연봉*/  
+	public List<ReviewUserVO> reviewSelectList2(ReviewUserVO reviewUserVO);
+	public int total2();
+	/*-면접후기*/  
+	public List<ReviewUserVO> reviewSelectList3(ReviewUserVO reviewUserVO);
+	public int total3();
 	
-	  public List<ReviewUserVO> reviewSelectList1(ReviewUserVO reviewUserVO);
-	  public int total1();
-	  
-	  public List<ReviewUserVO> reviewSelectList2(ReviewUserVO reviewUserVO);
-	  public int total2();
-	  
-	  public List<ReviewUserVO> reviewSelectList3(ReviewUserVO reviewUserVO);
-	  public int total3();
-	  
-	  public int reviewWriteForm(ReviewUserVO reviewUserVO);
+	/*글작성*/
+	public int Write(ReviewUserVO reviewUserVO);	
+	public int mWrite(ReviewUserVO reviewUserVO);
+	public int sWrite(ReviewUserVO reviewUserVO);
+	
+	/*리뷰상세보기*/
+	public ReviewUserVO reviewDetailForm(int rnum);	  
+	public ReviewUserVO mreviewDetailForm(int rnum);
+	public ReviewUserVO salDetailForm(int rnum);
 
+	/*조회수+1*/
+	public int plusBcnt(int rnum);
+	
+	/*리뷰 수정*/
+	public int reviewUpdate(ReviewUserVO reviewUserVO);
+	public int salUpdate(ReviewUserVO reviewUserVO);
+	public int mreviewUpdate(ReviewUserVO reviewUserVO);
+
+	/*리뷰 삭제*/
+	public int Delete(int rnum);
+	
+
+	
 		
 }
--- src/main/java/com/job/user/review/service/impl/ReviewUserDAO.java
+++ src/main/java/com/job/user/review/service/impl/ReviewUserDAO.java
@@ -13,42 +13,89 @@ public class ReviewUserDAO {
 	
 	@Autowired
 	private SqlSessionTemplate sessionTemplate;
-	
+	/*리뷰리스트*/
+	/*-기업리뷰*/
 	public List<ReviewUserVO> reviewSelectList1(ReviewUserVO reviewUserVO) {
 		System.out.println("StartDAO1");
 		List<ReviewUserVO> reviewSelectList = sessionTemplate.selectList("reviewUser.reviewList1", reviewUserVO);
 		return reviewSelectList;
-	}
-	
+	}	
 	public int total1() {
 		int total = (Integer)sessionTemplate.selectOne("reviewUser.total1");
 		return total;		
 	}
 	
+	/*-기업연봉*/ 
 	public List<ReviewUserVO> reviewSelectList2(ReviewUserVO reviewUserVO) {
 		System.out.println("StartDAO2");
 		List<ReviewUserVO> reviewSelectList = sessionTemplate.selectList("reviewUser.reviewList2", reviewUserVO);
 		return reviewSelectList;
-	}
-	
+	}	
 	public int total2() {
 		int total = (Integer)sessionTemplate.selectOne("reviewUser.total2");
 		return total;		
 	}
 	
+	/*-면접후기*/ 
 	public List<ReviewUserVO> reviewSelectList3(ReviewUserVO reviewUserVO) {
 		System.out.println("StartDAO3");
 		List<ReviewUserVO> reviewSelectList = sessionTemplate.selectList("reviewUser.reviewList3", reviewUserVO);
 		return reviewSelectList;
 	}
-	
 	public int total3() {
+		System.out.println("reviewUserServiceDAO total3");
 		int total = (Integer)sessionTemplate.selectOne("reviewUser.total3");
 		return total;		
 	}
 	
-	public int reviewWriteForm(ReviewUserVO reviewUserVO) {
-		return 0;
+	/*글작성*/
+	public int Write(ReviewUserVO reviewUserVO) {
+		return sessionTemplate.insert("reviewUser.reviewWrite", reviewUserVO);
+	}
+	public int mWrite(ReviewUserVO reviewUserVO) {
+		return sessionTemplate.insert("reviewUser.mreviewWrite", reviewUserVO);
+	}	
+	public int sWrite(ReviewUserVO reviewUserVO) {
+		return sessionTemplate.insert("reviewUser.salWrite", reviewUserVO);
+	}
+		
+	/*리뷰상세보기*/
+	public ReviewUserVO reviewDetailForm(int rnum) {
+		// TODO Auto-generated method stub
+		return sessionTemplate.selectOne("reviewUser.reviewDetail", rnum);
+	}
+	public ReviewUserVO mreviewDetailForm(int rnum) {
+		return sessionTemplate.selectOne("reviewUser.reviewDetail", rnum);
+	}
+	public ReviewUserVO salDetailForm(int rnum) {
+		return sessionTemplate.selectOne("reviewUser.reviewDetail", rnum);
+	}
+
+	/*조회수+1*/
+	public int plusBcnt(int rnum) {
+		// TODO Auto-generated method stub
+		return sessionTemplate.update("reviewUser.plusBcnt", rnum);
 	}
 	
+	/*리뷰 삭제*/
+	public int Delete(int rnum) {
+		// TODO Auto-generated method stub
+		return sessionTemplate.update("reviewUser.reviewDelete", rnum);
+	}
+	
+	/*리뷰 수정*/
+	public int Update(ReviewUserVO reviewUserVO) {
+		System.out.println("vo : update review : " + reviewUserVO.getReview());
+		System.out.println("vo : update mreview : " + reviewUserVO.getMreview());
+		return sessionTemplate.update("reviewUser.reviewUpdate", reviewUserVO);		
+	}
+	public int sUpdate(ReviewUserVO reviewUserVO) {
+		System.out.println("vo : update sal : " + reviewUserVO.getSal());
+		return sessionTemplate.update("reviewUser.salUpdate", reviewUserVO);		
+	}
+	public int mUpdate(ReviewUserVO reviewUserVO) {
+		System.out.println("vo : update review : " + reviewUserVO.getReview());
+		System.out.println("vo : update mreview : " + reviewUserVO.getMreview());
+		return sessionTemplate.update("reviewUser.mreviewUpdate", reviewUserVO);		
+	}
 }
